90541fc1b6145ec7264f89221d55d8746e9a4480
mmgwebi <mihlalimgwebi@gmail.com>
2024-11-07 20:01:14 +0200

    feat: :construction: externalize environment variables for runtime configuration
    - Moved environment configuration to an external JavaScript file (env.js) in the /assets folder.
    - This allows for dynamic configuration of environment variables without needing to rebuild the application.
    - While creating new builds is sometimes necessary in software development, doing so frequently can introduce significant drawbacks related to time consumption, resource usage, complexity in management, risk of errors, and reduced agility
    - Added apiUrl and debug settings to facilitate easier environment management.
    - As env.js is part of the /assets folder, it won't be cross-compiles but simply copied to the /dist directory and can be edited in clear text later.

---
+++ src/assets/env.js
@@ -0,0 +1,9 @@
+// https://pumpingco.de/blog/environment-variables-angular-docker/
+
+(function (window) {
+  window["env"] = window["env"] || {};
+
+  // Environment variables
+  window["env"]["apiUrl"] = "";
+  window["env"]["production"] = false;
+})(this);
